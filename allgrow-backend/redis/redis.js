342dead498e5a4086fa23d0413cf0ff118e51c66
yatinsingh2007 <yatin4591@gmail.com>
2026-07-17 11:09:14 +0530

    refactor: implement lazy initialization for Redis client using a getter function

--- allgrow-backend/redis/redis.js
+++ allgrow-backend/redis/redis.js
@@ -1,5 +1,12 @@
 const { Redis } = require("ioredis");
 
-const redis = new Redis(`${process.env.REDIS_URL}`)
+let redisInstance;
 
-module.exports = { redis }
+function getRedis() {
+  if (!redisInstance) {
+    redisInstance = new Redis(`${process.env.REDIS_URL}`);
+  }
+  return redisInstance;
+}
+
+module.exports = { getRedis };
